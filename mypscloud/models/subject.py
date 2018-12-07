dced6890400326bf92d60db6d8b3025195d220ad
wzy1221 <745018915@qq.com>
2018-12-07 11:59:47 +0000

    mypscloud

--- mypscloud/models/subject.py
+++ mypscloud/models/subject.py
@@ -3,12 +3,12 @@
 from odoo import api, fields, models
 
 class TrainingSubject(models.Model):
-    _name = 'test.subject'
+    _name = 'mypscloud.subject'
     _description = "科目"
 
     name = fields.Char(string='名称')
     person_id = fields.Many2one('res.partner', string='负责人')
-    lesson_ids = fields.One2many('test.lesson', 'subject_id', string='课程')
+    lesson_ids = fields.One2many('mypscloud.lesson', 'subject_id', string='课程')
     desc = fields.Text(string='描述')
 
 #  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:
